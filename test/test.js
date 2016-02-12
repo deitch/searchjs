30d32b8c22bb63cc4d60c7a77511af4ee4e9f903
Avi Deitcher <avi@deitcher.net>
2016-02-12 14:57:21 -0500

    Fix https://github.com/deitch/searchjs/issues/9 and add to tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,4 @@
 /*jslint node:true, nomen:true */
-/*global describe, it, before */
 var assert = require('assert'), search = require("../lib/searchjs");
 var runTest, data, searches;
 
@@ -8,34 +7,35 @@ data = [
 	{name:"Brian",age:30, email: "brian@searchjs.com",male:true,empty:"hello"},
 	{name:"Carrie",age:30, email: "carrie@searchjs.com",city:{"Montreal":true,"New York":false}},
 	{name:"David",age:35, email: "david@searchjs.com",male:true},
-	{name:"Alice",age:30, email: ["alice@searchjs.com","alice@gmail.com"], cars: [{brand: 'BMW', cds: [{title:'Best Of 2015'}, {title:'Best Of 2016'}]}, {brand: 'Porsche'}]}
+	{name:"Alice",age:30, email: ["alice@searchjs.com","alice@gmail.com"], cars: [{brand: 'BMW', cds: [{title:'Best Of 2015'}, {title:'Best Of 2016'}]}, {brand: 'Porsche'}]},
+	{name:"Other",id: 3,currentC: {cer: {id: 2}}}
 ];
 searches = [
 	{search: {name:"alice"}, results:[0,4]},
 	{search: {name:undefined}, results:[]},
-	{search: {empty:undefined}, results:[0,2,3,4]} ,
+	{search: {empty:undefined}, results:[0,2,3,4,5]} ,
 	{search: {name:"alic"}, results:[]},
 	{search: {name:"alic",_text:true}, results:[0,4]},
 	{search: {name:"alic",_word:true}, results:[]},
 	{search: {name:"alice",_word:true}, results:[0,4]},
 	{search: {name:"brian"}, results:[1]},
-	{search: {name:"alice",_not: true}, results:[1,2,3]},
+	{search: {name:"alice",_not: true}, results:[1,2,3,5]},
 	{search: {male:true}, results:[1,3]},
 	{search: {age:30,male:true}, results:[1]},
-	{search: {male:true,_not:true}, results:[0,2,4]},
+	{search: {male:true,_not:true}, results:[0,2,4,5]},
 	{search: {age:25}, results:[0]},
 	{search: {age:30}, results:[1,2,4]},
 	{search: {age:25, name: "Alice",email2:"foo@foo.com"},results: []},
 	{search: {age:35, name: "Alice", _join: "OR"},results: [0,3,4]},
-	{search: {age:35, name: "Alice", _not: true},results: [1,2]},
+	{search: {age:35, name: "Alice", _not: true},results: [1,2,5]},
 	{search: {terms: [{age:30,name:"Brian",_join:"AND"},{age:25}], _join:"OR"}, results:[0,1]},
 	{search: {email: "alice@searchjs.com"},results: [0,4]},
 	{search: {name: ["Brian","Carrie"]},results: [1,2]},
 	{search: {email: ["alice@searchjs.com","carrie@searchjs.com"]},results: [0,2,4]},
-	{search: {_not: true, name: ["Brian","Carrie"]},results: [0,3,4]},
-	{search: {_not:true, email: ["alice@searchjs.com","carrie@searchjs.com"]},results: [1,3]},
+	{search: {_not: true, name: ["Brian","Carrie"]},results: [0,3,4,5]},
+	{search: {_not:true, email: ["alice@searchjs.com","carrie@searchjs.com"]},results: [1,3,5]},
 	{search: {city:"Montreal"},results:[0,2]},
-	{search: {_not:true,city:"Montreal"},results:[1,3,4]},
+	{search: {_not:true,city:"Montreal"},results:[1,3,4,5]},
 	{search: {age:{from:30}},results:[1,2,3,4]},
 	{search: {age:{gt:30}},results:[1,2,3,4]},
 	{search: {age:{from:30,to:34}},results:[1,2,4]},
@@ -44,8 +44,8 @@ searches = [
 	{search: {age:{gt:25,lt:30}},results:[0,1,2,4]},
 	{search: {age:{to:29}},results:[0]},
 	{search: {age:{lt:29}},results:[0]},
-	{search: {_not:true,age:{to:29}},results:[1,2,3,4]},
-	{search: {_not:true,age:{from:30,to:34}},results:[0,3]},
+	{search: {_not:true,age:{to:29}},results:[1,2,3,4,5]},
+	{search: {_not:true,age:{from:30,to:34}},results:[0,3,5]},
 	{search: {"city.Montreal":"first"},results:[0]},
 	{search: {"city.Montreal":["first","abc"]},results:[0]},
 	{search: {"city:Montreal":"first",_separator: ':'},results:[0]},
@@ -60,7 +60,9 @@ searches = [
 	{search: {"cars.cds.title":"Best Of 2014"},results:[]},
 	{search: {"cars.cds.title":"Best Of 2015"},results:[4]},
 	{search: {"cars:cds:title":"Best Of 2015",_separator: ':'},results:[4]},
-	{search: {"cars.cds.title":"Best Of 2016"},results:[4]}
+	{search: {"cars.cds.title":"Best Of 2016"},results:[4]},
+	{search: {"currentC.cer.id": [1, 2]},results: [5]},
+	{search: {"currentC.cer.id": [2, 1]},results: [5]}
 ];
 
 
